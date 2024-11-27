c0a8769b71b70b2a26675ccbb4a7206652c86a5d
Tatsuya <a.tatsu0117@gmail.com>
2024-11-28 00:47:09 +0900

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -17,5 +19,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap()],
 });
---
+++ src/data/metadata.js
@@ -0,0 +1,25 @@
+export const SITE_NAME = "夢百科";
+export const OG_IMAGE_URL = "/ogp.png";
+
+export const TOP_METADATA = {
+  title: SITE_NAME,
+  description: "夢百科は、夢の解釈や夢占いに関する情報を提供するサイトです。",
+};
+
+export const ARTICLE_METADATA = {
+  title: `夢占い | ${SITE_NAME}`,
+  description: "夢占いの記事一覧です。",
+};
+
+export const ARTICLE_SINGLE_METADATA = {
+  title: `夢占い | ${SITE_NAME}`,
+};
+
+export const KEYWORD_METADATA = {
+  title: `キーワード一覧 | ${SITE_NAME}`,
+  description: "夢占いのキーワード一覧です。",
+};
+
+export const KEYWORD_SINGLE_METADATA = {
+  title: `キーワード一覧 | ${SITE_NAME}`,
+};
