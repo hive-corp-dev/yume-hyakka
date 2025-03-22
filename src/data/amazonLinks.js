a2bf9aa286b81b890ab3055f254b36540e8f7bba
Tatsuya <a.tatsu0117@gmail.com>
2025-03-23 01:13:12 +0900

    added column

--- src/data/amazonLinks.js
+++ src/data/amazonLinks.js
@@ -1,4 +1,12 @@
 export const amznLink = {
   treasureShipCard: "https://amzn.to/3W04olw",
   lavenderOilResult: "https://amzn.to/42cJ40b",
+  tentialSweats: "https://amzn.to/4bTpSar",
+  tentialBedPad: "https://amzn.to/4hyLuu2",
+  dreamJournal: "https://amzn.to/4iu4SJS",
+  tenYearsDiary: "https://amzn.to/4izMCyU",
+  aromaOilForSleep: "https://amzn.to/41J4IqQ",
+  andGoodnight: "https://amzn.to/4hHYnC8",
+  dreamCatcher: "https://amzn.to/4hAcKbv",
+  whiteSage: "https://amzn.to/4iuF8gu",
 };
